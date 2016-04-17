e2ed82687985089aca35b3a67ac2a4280644eb11
irinaterribilini <irina.terribilini@students.fhnw.ch>
2016-04-17 09:15:46 +0200

    try implementing the marker

--- site/controllers/where.js
+++ site/controllers/where.js
@@ -21,10 +21,31 @@ function initialize() {
     var getPosition = function (position) {
         console.log(position.coords.latitude, position.coords.longitude);
         map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
+
     }
     window.navigator.geolocation.getCurrentPosition(getPosition);
 }
-    initialize();
+
+    function initMap(getWhat) {
+        var WhatLatLng = {lat: getWhat.latitude(), lng: getWhat.longitude()};
+
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('where-screen'), {
+            center: WhatLatLng,
+            scrollwheel: true,
+            zoom: 15
+        });
+
+        // Create a marker and set its position.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: WhatLatLng,
+            title: 'MC Donalds (Brugg-City)!'
+        });
+    }
+
+initialize();
+initMap();
 
 
 
