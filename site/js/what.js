326c200369ef97adad1f16942f949eff8ac9b9a2
LukasWilllinFHNW <lukas.willin@students.fhnw.ch>
2016-04-19 12:04:02 +0200

    smaller changes

--- site/js/what.js
+++ site/js/what.js
@@ -11,11 +11,23 @@ console.log("Loaded what.js");
  * Klick Event Listener für Such Button. Sucht nach dem entsprechenden Tag und gibt einen Fehler aus falls keine
  * gefunden worden sind. Ansosten werden die Markers gesetzt.
  */
-$("p").click(function(event) {
-    var tag = $("#SearchTag").attr('value');
+$("#Search").click(function(event) {
 
-    // TODO
+    event.preventDefault();
 
-    // Übergebe Tag für andere Funktion
+    // Read value from
+    var tag = $("#SearchTag").val();
+
+    // Create array from text
+    var tagArray = tag.split(',').map( function(s) {
+        return s.trim();
+    })
+
+    console.log(tagArray);
+
+    // clear values from input
+    $("#SearchTag").val("");
+
+    // Hand over tags to the where.js controller
     setMarkers(tag);
 });
